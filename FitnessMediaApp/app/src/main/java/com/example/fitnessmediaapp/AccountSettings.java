12d3add939ee4f0ca3b38d62136a5d7175e11292
ahmadalsabagh <ahmad-alsabagh@hotmail.com>
2020-11-02 18:51:16 -0500

    Created the account settings UI, bottom bar not yet implemented though

--- FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/AccountSettings.java
+++ FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/AccountSettings.java
@@ -5,17 +5,17 @@ import android.os.Bundle;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+public class AccountSettings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_account_settings);
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setTitle("Register");  // provide compatibility to all the versions
+            actionBar.setTitle("Account Settings");  // provide compatibility to all the versions
         }
     }
 }
