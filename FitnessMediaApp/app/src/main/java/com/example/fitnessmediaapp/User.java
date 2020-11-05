d20237e0e6b971edc329e38f518af26755b36b6c
ahmadalsabagh <ahmad-alsabagh@hotmail.com>
2020-11-04 19:57:35 -0500

    Created the User Entity

---
+++ FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/User.java
@@ -0,0 +1,89 @@
+package com.example.fitnessmediaapp;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+
+@Entity(indices = {@Index(value = {"username"},
+        unique = true)}, tableName = "s") //CHANGE TABLE NAME & verify username column matches
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "s") //CHANGE!
+    private String username;
+
+    @NonNull
+    @ColumnInfo(name = "s") //CHANGE!
+    private String firstName;
+
+    @NonNull
+    @ColumnInfo(name = "s") //CHANGE!
+    private String lastName;
+
+    @NonNull
+    @ColumnInfo(name = "s") //CHANGE!
+    private String password;
+
+    @Ignore
+    public User(int id, @NonNull String username, @NonNull String firstName, @NonNull String lastName, @NonNull String password){
+        this.id = id;
+        this.firstName = firstName;
+        this.password = password;
+        this.lastName = lastName;
+        this.username = username;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setUsername(@NonNull String username) {
+        this.username = username;
+    }
+
+    public void setId(@NonNull int id){
+        this.id = id;
+    }
+
+    public void setFirstName(@NonNull String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setPassword(@NonNull String password) {
+        this.password = password;
+    }
+
+    public void setLastName(@NonNull String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean checkPassword(@NonNull String password){
+        if(!this.password.equals(password)){
+            return false;
+        }
+        return true;
+    }
+
+
+
+
+}
