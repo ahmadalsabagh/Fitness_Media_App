9f3204faa7e0b671cc0585f7aa6734252dd9fb1b
Daniel <daniel.lodge23@gmail.com>
2020-11-05 12:19:43 -0500

    Added testing code to check that records can be created in the database producing an error that may be related to MultiDex issues

--- FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/MainActivity.java
+++ FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.fitnessmediaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    public static final String FirstN_Key = "firstName";
+    public static final String LastN_Key = "lastName";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +29,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button register = findViewById(R.id.registerBtn);
         Button login = findViewById(R.id.loginBtn);
+
+        Map<String, Object> user = new HashMap<>();
+        user.put(FirstN_Key, "Daniel");
+        user.put(LastN_Key, "Lodge");
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "Document added with ID" + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
