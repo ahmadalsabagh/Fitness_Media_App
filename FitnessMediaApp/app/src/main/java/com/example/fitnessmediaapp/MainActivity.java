b9302e5a665c9ccfb469b4655e0c6b1dd8458778
Daniel <daniel.lodge23@gmail.com>
2020-11-06 21:12:53 -0500

    removed database test code. Added code to register a user. register.OnClickListener crashes application

--- FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/MainActivity.java
+++ FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/MainActivity.java
@@ -19,9 +19,6 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity  {
     private static final String TAG = "MainActivity";
-    public static final String FirstN_Key = "firstName";
-    public static final String LastN_Key = "lastName";
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,25 +26,6 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
         Button register = findViewById(R.id.registerBtn);
         Button login = findViewById(R.id.loginBtn);
-
-        Map<String, Object> user = new HashMap<>();
-        user.put(FirstN_Key, "Daniel");
-        user.put(LastN_Key, "Lodge");
-
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "Document added with ID" + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
