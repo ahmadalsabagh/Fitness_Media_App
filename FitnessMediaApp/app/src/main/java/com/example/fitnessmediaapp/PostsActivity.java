39e33d3b8e2e7adaff4af7b570a087e575a66981
Daniel <daniel.lodge23@gmail.com>
2020-11-15 23:09:13 -0500

    All fields of the post collection are retrieved. Need to save the fields into strings, then as objects in an arraylist.

--- FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/PostsActivity.java
+++ FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/PostsActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,9 +17,13 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class PostsActivity extends AppCompatActivity {
     private final LinkedList<String> mPostsList = new LinkedList<>();
@@ -27,50 +32,63 @@ public class PostsActivity extends AppCompatActivity {
 //    private TextView textViewData;
     public static final String Key = "post";
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private DocumentReference postRef = db.document("posts/myPost");
+//    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    private DocumentReference postRef = db.document("posts/myPost");
     private static final String TAG = "PostsActivity";
     public static String postData;
 
-
-
-
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        postRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+//                if (e != null) {
+//                    //debug comment
+//                    Toast.makeText(PostsActivity.this, "Error while loading!", Toast.LENGTH_SHORT).show();
+//                    Log.d(TAG, e.toString());
+//                    return;
+//                }
+//                if (documentSnapshot.exists()) {
+//                    postData = documentSnapshot.getString(Key);
+//
+//                }
+//            }
+//        });
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.posts_recyclerview);
 
-        postRef.get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        FirebaseFirestore.getInstance()
+                .collection("posts")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-                            // we are taking the information from database, storing it into a string, then adding it to a linked list, so
-                            // posts adapter can display the data on the screen.
-                            postData = documentSnapshot.getString(Key);
-                            Log.d(TAG, "onSuccess: retrieving data");
-                            //Map<String, Object> note = documentSnapshot.getData();
-//                            textViewData.setText("Title: " + postData);
-                        } else {
-                            Log.d(TAG, "onFailure: Document Doesn't exist");
-
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentData, @Nullable FirebaseFirestoreException error) {
+                        if(error != null){
+                            Log.e(TAG, "onEvent: ", error);
+                            return;
                         }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(PostsActivity.this, "Error!", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, e.toString());
+                        if(queryDocumentData != null){
+                            List<DocumentSnapshot> snapshotList = queryDocumentData.getDocuments();
+                            for(DocumentSnapshot x : snapshotList){
+                                Log.d(TAG, "onEvent: " + x.getData());
+                            }
+                        } else{
+                            Log.e(TAG, "onEvent: query snapshot was null");
+                        }
                     }
                 });
 
 
 
-        for (int i = 0; i < 30; i++) {
-            mPostsList.addLast(postData);
-        }
+//        Provides dummy data to linked list
+//        for (int i = 0; i < 30; i++) {
+//            mPostsList.addLast(postData);
+//        }
 
         mRecyclerView = findViewById(R.id.recyclerView);
         mAdapter = new PostsAdapter(this, mPostsList);
