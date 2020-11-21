f27a13a900eb4f63923460a8d6f5136bfa4935df
Daniel <daniel.lodge23@gmail.com>
2020-11-20 23:48:56 -0500

    Post button works using an alert dialog. New posts update in real time. Need to implement more fields and fix UI

--- FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/PostsActivity.java
+++ FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/PostsActivity.java
@@ -1,15 +1,18 @@
 package com.example.fitnessmediaapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -36,6 +39,20 @@ public class PostsActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     PostsAdapter postsRecyclerViewAdapter;
 
+
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    private DocumentReference postRef = db.document("posts/myPost");
+    private static final String TAG = "PostsActivity";
+    public static String postContent;
+    public static String postLocation;
+    public static String postTime;
+    public static String postUsername;
+    public static String postContentString;
+    //    public static String toast_message = "heloooooooooooo";
+    //Registering the listener
+    private ListenerRegistration snapShotActivationController;
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -49,6 +66,69 @@ public class PostsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.posts_recyclerview);
         recyclerView = findViewById(R.id.recyclerViewPosts);
+
+        Button btnPost = findViewById(R.id.createPostBtn);
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlertDialog();
+//                final Map<String, Object> posts = new HashMap<>();
+//                TextView postContentToUpload = findViewById(R.id.txtPostContent);
+//                postContentString = postContentToUpload.getText().toString();
+//                posts.put("content", postContentString);
+//
+//                db.collection("posts")
+//                        .add(posts)
+//                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                            @Override
+//                            public void onSuccess(DocumentReference documentReference) {
+//                                Log.d(TAG, "Document added with ID" + documentReference.getId());
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.w(TAG, "Error adding document", e);
+//                            }
+//                        });
+            }
+        });
+
+
+    }
+
+    private void showAlertDialog() {
+        final EditText postEditText = new EditText(this);
+        new AlertDialog.Builder(this)
+                .setTitle("Make a New Fitness Post!")
+                .setView(postEditText)
+                .setPositiveButton("Post", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d(TAG, "onClick: " + postEditText.getText());
+                        final Map<String, Object> posts = new HashMap<>();
+                posts.put("content", postEditText.getText().toString());
+
+                db.collection("posts")
+                        .add(posts)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "Document added with ID" + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
     }
 
     @Override
@@ -57,14 +137,14 @@ public class PostsActivity extends AppCompatActivity {
         if(postsRecyclerViewAdapter != null){
             postsRecyclerViewAdapter.stopListening();
         }
-//        snapShotActivationController.remove();
     }
 
     //----------ONCLICK for uploading posts
-    public void uploadPost(View v){
+//    public void uploadPost(View v){
+//        setContentView(R.layout.posts_recyclerview);
 //        final Map<String, Object> posts = new HashMap<>();
-//        final EditText postContent = findViewById(R.id.txtPostContent);
-//        postContentString = postContent.getText().toString();
+//        EditText postContentToUpload = findViewById(R.id.txtPostContent);
+//        postContentString = postContentToUpload.getText().toString();
 //        posts.put("content", postContentString);
 //
 //        db.collection("posts")
@@ -81,7 +161,8 @@ public class PostsActivity extends AppCompatActivity {
 //                        Log.w(TAG, "Error adding document", e);
 //                    }
 //                });
-    }
+//
+//    }
 
     private void initRecyclerView(){
 
