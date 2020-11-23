aab090bae1d8c63e2f0dd6a8220c4a37cc2696ef
Daniel <daniel.lodge23@gmail.com>
2020-11-23 10:54:23 -0500

    Fixed additional Manifest Issue

--- FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/LoginActivity.java
+++ FitnessMediaApp/app/src/main/java/com/example/fitnessmediaapp/LoginActivity.java
@@ -99,7 +99,7 @@ public class LoginActivity extends AppCompatActivity {
                                     System.out.println("It passes the test of authentication");
 //                                    Intent intent = new Intent(getApplicationContext(), StepCounter.class);
 //                                    startActivity(intent);
-                                    Intent intent = new Intent(getApplicationContext(), PostsActivity.class);
+                                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                                     startActivity(intent);
                                 }
                             }
